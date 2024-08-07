e477289a59b4dea6d0a1e4eb6262590628d44442
darriola2021670 <darriola-2021670@kinal.edu.gt>
2024-08-07 08:13:53 -0600

    build(master):agregue proyecto a la rama maestra

---
+++ src/main/java/com/marcobolanos/system/SpringBootSistemaRhApplication.java
@@ -0,0 +1,13 @@
+package com.marcobolanos.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSistemaRhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSistemaRhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcobolanos/system/SpringBootSistemaRhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcobolanos.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSistemaRhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
