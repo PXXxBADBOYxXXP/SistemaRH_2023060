75c5b3a58f8461e17b6a80c4041bbd36cc337586
darriola2021670 <darriola-2021670@kinal.edu.gt>
2024-08-07 10:44:16 -0600

    feat(EmpleadosService):cree el metodo listarEmpleados

--- src/main/java/com/marcobolanos/system/service/EmpleadosService.java
+++ src/main/java/com/marcobolanos/system/service/EmpleadosService.java
@@ -1,8 +1,40 @@
 package com.marcobolanos.system.service;
 
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.marcobolanos.system.model.Empleados;
+import com.marcobolanos.system.repository.EmpleadosRepositorio;
+// Me va a hacer comunicacion de esta clase (SERVICIO) en bean(model)
+@Service
 public class EmpleadosService implements IEmpleadosService{
+    @Autowired
+    // variable para almacenar el autowired
+    private EmpleadosRepositorio empleadosRepositorio;
+    @Override
+    public List<Empleados> listarEmpleados() {
+        return empleadosRepositorio.findAll();
+    }
+
+    @Override
+    public Empleados buscarEmpleados(Integer idEmpleado) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buscarEmpleados'");
+    }
+
+    @Override
+    public Empleados guardarEmpleados(Empleados empleados) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'guardarEmpleados'");
+    }
+
+    @Override
+    public void eliminarEmpleados(Empleados empleados) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'eliminarEmpleados'");
+    }
 
     
 }
