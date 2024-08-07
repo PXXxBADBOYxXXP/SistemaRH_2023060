28e3259d7decc2fe986d43829e266272a7b63c1e
darriola2021670 <darriola-2021670@kinal.edu.gt>
2024-08-07 11:28:35 -0600

    feat(VacantesRepositorio):cree el repositorio de la entidad vacantes

---
+++ src/main/java/com/marcobolanos/system/repository/VacantesRepositorio.java
@@ -0,0 +1,9 @@
+package com.marcobolanos.system.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.marcobolanos.system.model.Vacantes;
+
+public interface VacantesRepositorio extends JpaRepository<Vacantes, Integer>{
+
+}
