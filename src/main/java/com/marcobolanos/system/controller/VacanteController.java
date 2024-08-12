aef01b47f3203fd8906c7bc9f0bb0261732b5f12
PXXxBADBOYxXXP <marcojosepro2007@gmail.com>
2024-08-12 14:29:56 -0600

    feat(VacanteController):agregue el metodo obtener vacantes

--- src/main/java/com/marcobolanos/system/controller/VacanteController.java
+++ src/main/java/com/marcobolanos/system/controller/VacanteController.java
@@ -1,5 +1,32 @@
 package com.marcobolanos.system.controller;
+import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.marcobolanos.system.model.Vacantes;
+import com.marcobolanos.system.service.IVacantesService;
+
+@RestController
+@RequestMapping("rh-vacante")
 public class VacanteController {
 
+    private static final Logger logger =LoggerFactory.getLogger(VacanteController.class);
+
+    @Autowired
+    private IVacantesService iVacantesService;
+
+    @GetMapping("/vacantes")
+
+    public List<Vacantes> obtenerVacantes (){
+        var vacante2 = iVacantesService.listarVacantes();
+        vacante2.forEach((vacante ->  logger.info(vacante.toString())));
+        return vacante2;
+    }
+
+    
 }
