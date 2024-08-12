1bb1b9aec1cc32f10810adf7e9518a186e7203e4
PXXxBADBOYxXXP <marcojosepro2007@gmail.com>
2024-08-12 14:35:05 -0600

    feat

--- src/main/java/com/marcobolanos/system/controller/VacanteController.java
+++ src/main/java/com/marcobolanos/system/controller/VacanteController.java
@@ -5,6 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +30,10 @@ public class VacanteController {
         return vacante2;
     }
 
-    
+    @PostMapping("/vacantesA")
+    public Vacantes agregarVacantes (@RequestBody Vacantes vacantes){
+        logger.info("vacante agregada");
+        return iVacantesService.guardarVacantes(vacantes);
+    }
+
 }
